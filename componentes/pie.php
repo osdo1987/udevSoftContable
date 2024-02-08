105f58e6201d781f88933c6ee7cfdcf4bcf7b5c8
oscarDorado <odorado@tiaxa.net>
2024-02-08 04:34:28 -0500

    se crea carpeta inicial de navbar y pie de pagina

---
+++ componentes/pie.php
@@ -0,0 +1,17 @@
+
+<footer class="bg-light text-center text-lg-start">
+  <div class="container p-4">
+   
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2024 Osdosoft.com:
+    <a class="text-dark" href="#">Enlace de Ejemplo</a>
+</div>
+
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
