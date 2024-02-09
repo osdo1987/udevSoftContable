015f2234a2ec76f92c328c58d576db44764b21eb
Khalid <jrkhalid3@gmail.com>
2024-02-09 17:21:04 -0500

    actualizacion de archivos

--- formulario/Controlador.php
+++
@@ -1,7 +0,0 @@
-<?php
-$codigo_descuento = $_GET['codigo_descuento'];
-$valor_descuento = $_GET['valor_descuento'];
-
-echo "codigo descuento: ",$codigo_descuento;
-echo "codigo valor",$valor_descuento;
-?>
