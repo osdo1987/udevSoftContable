72bf7d9218ec03ad00421da4789a21426a8ea5b6
Juan Esteban Florez <esteban200506@gmail.com>
2024-02-09 15:36:20 -0500

    actualizacion de formulario

--- tipo_movimiento/tipo_movimiento.view.php
+++ tipo_movimiento/tipo_movimiento.view.php
@@ -8,7 +8,13 @@
 </head>
 <body>
 
-<form name='form_tipo_movimiento' method='post' action='tipo_movimiento.controller.php'>
+<div class="card">
+  <div class="card-header">
+    tipo movimiento
+  </div>
+  <div class="card-body">
+
+  <form name='form_tipo_movimiento' method='post' action='tipo_movimiento.controller.php'>
   <div class="mb-3">
     <label for="Tipo_movimiento" class="form-label">Tipo de movimiento</label>
     <input type="text" class="form-control" id="tipo_movimiento" name='tipo_movimiento' value="">
@@ -16,6 +22,9 @@
 
   <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+    
+  </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
