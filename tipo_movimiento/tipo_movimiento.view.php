fcd0cd97e938655565de44d1ee003c12aa43214d
Juan Esteban Florez <esteban200506@gmail.com>
2024-02-09 17:22:52 -0500

    se añaden el navbar y footer

--- tipo_movimiento/tipo_movimiento.view.php
+++ tipo_movimiento/tipo_movimiento.view.php
@@ -7,6 +7,7 @@
     <title>Tipo movimiento</title>
 </head>
 <body>
+<?php include_once '../componentes/navbar.php' ?>
 
 <div class="card">
   <div class="card-header">
@@ -25,7 +26,7 @@
     
   </div>
 </div>
-
+<?php include_once '../componentes/pie.php'?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
