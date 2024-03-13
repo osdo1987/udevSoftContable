18625bcbb8c129978abffd270a532388095fe5a2
SamuelTrujillo <samuelitoguapacha@gmail.com>
2024-03-13 10:46:58 -0500

    mernning

--- Convenios/Convenios.controller.php
+++ Convenios/Convenios.controller.php
@@ -65,7 +65,94 @@ function editar($conexion){
 
 
 }
+function obtener_registros($conexion)
+{
+    $query = "";
+    $salida = array();
+    $query = "SELECT * FROM convenio";
 
+    if (isset($_POST["search"]["value"])) {
+        $query .= ' WHERE descripcion_convenio LIKE "%' . $_POST["search"]["value"] . '%" ';
+    }
+
+    if (isset($_POST["order"])) {
+        $query .= ' ORDER BY ' . $_POST['order']['0']['column'] . ' ' . $_POST["order"][0]['dir'] . ' ';
+    } else {
+        $query .= ' ORDER BY codigo_convenio DESC ';
+    }
+
+    if (isset($_POST['length']) && isset($_POST['start'])) {
+        $query .= 'LIMIT ' . ($_POST["start"]) . ',' . $_POST["length"];
+    }
+
+    $stmt = $conexion->prepare($query);
+
+    try {
+
+        $stmt->execute();
+        $resultado = $stmt->fetchAll();
+        $datos = array();
+        $filtered_rows = $stmt->rowCount();
+
+        $draw = isset($_POST['draw']) ? intval($_POST['draw']) : 0;
+        foreach ($resultado as $fila) {
+            $sub_array = array();
+            $sub_array[] = $fila["codigo_convenio"];
+            $sub_array[] = $fila["descripcion_convenio"];
+            $sub_array[] = $fila["valor_total_convenio"];
+            $sub_array[] = $fila["saldo_convenio"];
+            $sub_array[] = $fila["codigo_carrera"];
+            $sub_array[] = $fila["codigo_estudiante"];
+            $sub_array[] = $fila["estado"];
+
+            $sub_array[] = '<button type="button" data-bs-toggle="modal" data-bs-target="#modalConvenio" name="editar" id="' . $fila["codigo_convenio"] . '" class="btn btn-warning bi bi-pencil-square editar"></button>';
+
+            $datos[] = $sub_array;
+        }
+
+        $salida = array(
+            "draw" => $draw,
+            "recordsTotal" => $filtered_rows,
+            "recordsFiltered" => obtener_todos_registros(),
+            "data" => $datos
+        );
+
+        echo json_encode($salida);
+    } catch (Exception $e) {
+        echo "Error en la consulta: " . $e->getMessage();
+    }
+}
+
+function obtener_registro($conexion)
+{
+
+    $salida = array();
+
+    try {
+        $stmt = $conexion->prepare("SELECT * FROM convenio WHERE codigo_convenio = :codigo_convenio LIMIT 1");
+        $stmt->bindParam(':codigo_convenio', $_POST['codigo_convenio'], PDO::PARAM_INT);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            $salida = $resultado;
+        } else {
+            $salida["error"] = "No se encontraron resultados";
+        }
+    } catch (PDOException $e) {
+        $salida["error"] = "Error en la ejecución de la consulta: " . $e->getMessage();
+    }
+    echo json_encode($salida);
+}
+
+function obtener_todos_registros()
+{
+    include('../conexion.php');
+    $stmt = $conexion->prepare('SELECT * FROM convenio');
+    $stmt->execute();
+    $resultado = $stmt->fetch();
+    return $stmt->rowCount();
+}
 
 
 
