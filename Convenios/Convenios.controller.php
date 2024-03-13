d74c74111ba31d7d525cb536193463fa2b6e6aa3
SamuelTrujillo <samuelitoguapacha@gmail.com>
2024-03-12 23:23:27 -0500

    nuevo

---
+++ Convenios/Convenios.controller.php
@@ -0,0 +1,29 @@
+<?php
+
+include("../conexion.php");
+
+@$action = $_POST["operacion"];
+main($action, $conexion);
+
+function main($action, $conexion)
+{
+    switch ($action) {
+        case 'crear':
+            crear($conexion);
+            break;
+        case 'editar':
+            editar($conexion);
+            break;
+        case 'borrar':
+            borrar($conexion);
+            break;
+        case 'obtener_registro':
+            obtener_registro($conexion);
+            break;
+        default:
+        obtener_registros($conexion);
+            break;
+    }
+}
+
+?>
