ada3771a6dacff38c20b7496f4cd91bb5e4b9a03
SamuelTrujillo <samuelitoguapacha@gmail.com>
2024-03-13 10:30:50 -0500

    desarrollo funcionalidad crear y editar en proceso

--- Convenios/Convenios.controller.php
+++ Convenios/Convenios.controller.php
@@ -25,4 +25,51 @@ function main($action, $conexion)
             break;
     }
 }
+
+function crear ($conexion)
+
+{
+
+$stmt = $conexion->prepare("INSERT INTO convenios(codigo_convenio, descripcion_convenio, valor_total_convenio, saldo_convenio, codigo_carrera, codigo_institucion, codigo_tipo_convenio, codigo_estudiante, estado)VALUES(:codigo_convenio, :nombre_convenio, :valor_total_convenio, :saldo_convenio, :codigo_carrera, :codigo_institucion, :codigo_tipo_convenio, :codigo_estudiante, :estado )");
+
+$resultado = $stmt->execute(
+
+    array(
+        'codigo_convenio' => $_POST["codigo_convenio"],
+        'descripcion_convenio' => $_POST["nombre_convenio"],
+        'valor_total_convenio' => $_POST["valor_total_convenio"],
+        'saldo_convenio' => $_POST["saldo_convenio"],
+        'codigo_carrera' => $_POST["codigo_carrera"],
+        'codigo_institucion' => $_POST["codigo_institucion"],
+        'codigo_tipo_convenio' => $_POST["codigo_tipo_convenio"],
+        'codigo_estudiante' => $_POST["codigo_estudiante"],
+        'estado' => $_POST["estado"]
+        )
+    );
+    if(!empty($resultado)) {
+        echo 'Registro creado';
+    } else {
+        echo 'Registro no creado';
+    }
+
+}
+
+function editar($conexion){
+    $stmt = $conexion->("UPDATE convenios SET descripcion_convenio=:nombre_convenio, valor_total_convenio=:valor_total_convenio, saldo_convenio=:saldo_convenio, codigo_carrera=:codigo_carrera, 
+    codigo_institucion=:codigo_institucion, codigo_tipo_convenio=:codigo_tipo_convenio, codigo_estudiante=:codigo_estudiante, estado=:estado WHERE codigo_convenio=:codigo_convenio");
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
 ?>
