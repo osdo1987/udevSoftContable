f3470eb2268ecc5dd3f2e498095222a2ab4b59f7
Jfan448 <jfan448@outlook.es>
2024-03-12 22:57:28 -0500

    Creacion modal de pagos (boton "Pagos" temporal)

--- Convenios/Convenios.view.php
+++ Convenios/Convenios.view.php
@@ -35,6 +35,7 @@
     </div>
   </div>
   <div class="text-center"> 
+        <button type="button" class="btn btn-primary" id="btn-pagar-modal">Pagos</button>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCrearConvenio" id="botonCrear">
             Crear
         </button>
@@ -89,6 +90,69 @@
       </div>
     </div>
   </div>
+
+ <!-- Modal -->
+ <div class="modal fade" id="nuevoModal" tabindex="-1" aria-labelledby="nuevoModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+  <div class="modal-dialog modal-dialog-scrollable modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="nuevoModalLabel">Pagos</h5>
+        <button type="button" class="btn btn-light btn-close" style="position: absolute; top: 0.5rem; right: 1.5rem; padding: 0.5rem 1rem; border: 0; background: 0; font-size: 1.5rem; cursor: pointer; outline: 0;" data-bs-dismiss="modal" aria-label="Close">
+          <i class="bi bi-x-lg"></i>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row w-100">
+          <div class="col-md-6">
+            <h5>Código: <span id="codigo"></span></h5>
+          </div>
+          <div class="col-md-6 text-end">
+            <img src="" alt="Imagen del estudiante" id="imagen" class="img-fluid">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h5>Nombre: <span id="nombre"></span></h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h5>Cédula: <span id="cedula"></span></h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h5>Fecha de nacimiento: <span id="fecha_nacimiento"></span></h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h5>Carrera: <span id="carrera"></span></h5>
+          </div>
+        </div>
+        <br>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>P</th>
+              <th>Código</th>
+              <th>Fecha</th>
+              <th>Saldo</th>
+              <th>Seleccionar</th>
+            </tr>
+          </thead>
+          <tbody id="tabla-pagos">
+            <!-- Aquí se mostrarán los pagos -->
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="btn-pagar">Pagar</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
     
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -97,6 +161,13 @@
   <!-- Bootstrap JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script type="text/javascript">
+    $(document).ready(function() {
+  $('#nuevoModal').modal('hide');
+
+  document.getElementById('btn-pagar-modal').addEventListener('click', function() {
+    $('#nuevoModal').modal('show');
+  });
+});
     $(document).ready(function() {
       $("#botonCrear").click(function() {
         $("#formulario")[0].reset();
@@ -131,6 +202,60 @@
         
         });
     });
+
+    $(document).on('submit', '#formulario', function(event) {
+        event.preventDefault();
+        var codigo_convenio = $("#codigo_convenio").val();
+        var nombre_convenio = $("#nombre_convenio").val();
+        var tipo_convenio = $("#tipo_convenio").val();
+        var convenio_convenio = $("#convenio_convenio").val();
+        var valor_total_convenio = $("#valor_total_convenio").val();
+        
+        if (codigo_convenio != '' && nombre_convenio != '' && tipo_convenio != '' && convenio_convenio != '' && valor_total_convenio != '') {
+          $.ajax({
+            url: "Convenios.controller.php",
+            method: 'POST',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function(data) {
+              alert(data);
+              $('#formulario')[0].reset();
+              $('.modal-title').modal('hide');
+              dataTable.ajax.reload();
+            }
+          });
+        } else {
+          alert("Todos los campos son obligatorios");
+        }
+      });
+
+
+    // Funcionalidad de editar
+    $(document).on('click', '.editar', function() {
+        var codigo_convenios = $(this).attr("id");
+        $.ajax({
+          url: "Convenios.controller.php",
+          method: "POST",
+          data: {codigo_convenio: codigo_convenio,operacion:'obtener_registro'},
+          dataType: "json",
+          success: function(data) {
+            $('#modalCrearConvenio').modal('show');
+            $('#codigo_convenio').val(data.codigo_convenio);
+            $('#nombre_convenio').val(data.nombre_convenio);
+            $('#tipo_convenio').val(data.tipo_convenio);
+            $('#convenio_convenio').val(data.convenio_convenio);
+            $('#valor_total_convenio').val(data.valor_total_convenio);
+            $('.modal-title').text("Editar servicio");
+            $('#action').val("Editar")
+            $('#operacion').val("editar");
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+          }
+        });
+      });
+
   </script>
   
 </body>
