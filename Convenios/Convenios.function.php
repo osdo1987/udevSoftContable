b6926c0a71ba2e957820248042e0d469888013cf
SamuelTrujillo <samuelitoguapacha@gmail.com>
2024-08-11 16:37:16 -0500

    creacion nuevo documentro .function

---
+++ Convenios/Convenios.function.php
@@ -0,0 +1,57 @@
+<?php
+
+include("../conexion.php");
+
+@$action = $_POST["operacion"];
+
+main($action, $conexion);
+
+function main($action, $conexion)
+{
+    switch ($action) {
+        case 'registro_individual':
+            Registro_individual($conexion);
+            break;
+       
+            
+      
+        default:
+            
+            break;
+
+
+    }
+}
+
+function Registro_individual($conexion){
+    if ($conexion) {
+        try {
+    
+          
+            $query = "SELECT estudiantes.codigo_estudiante, estudiantes.nombre_estudiante, estudiantes.apellidos_estudiante, estudiantes.fecha_nacimiento_estudiante, estudiantes.imagen, servicios.codigo_servicio, servicios.descripcion_servicio
+            FROM convenio 
+            INNER JOIN estudiantes 
+            ON convenio.codigo_estudiante = estudiantes.codigo_estudiante 
+            INNER JOIN servicios
+            ON convenio.codigo_servicio = servicios.codigo_servicio 
+            WHERE convenio.codigo_estudiante = 10";
+    
+            $resultadoDato = $conexion->query($query);
+            
+    
+    
+            while ($row = $resultadoDato->fetch(PDO::FETCH_ASSOC)) {
+                $codigo_estudi = $row['codigo_estudiante'];
+                $nombre_est = $row['nombre_estudiante'];
+                $apellidos_est = $row['apellidos_estudiante'];
+                $fecha_naci_est = $row['fecha_nacimiento_estudiante'];
+                $carrera_est= $row['descripcion_servicio'];
+                $imagen_est= $row['imagen'];
+               
+            }
+        } catch (PDOException $e) {
+            echo "error al ejecutar " . $e->getMessage();
+        }
+    }
+
+}
